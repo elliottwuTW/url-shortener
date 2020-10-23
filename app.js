70d81567f3577036762efb4a36ca5d8589fc79d2
elliottwuTW <elliott10009@gmail.com>
2020-10-23 14:49:59 +0800

    feat: add express-handlebars and create needed pages

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({
+  defaultLayout: 'layout',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('app created!')
+  res.render('index')
 })
 
 app.listen(port, () => {
